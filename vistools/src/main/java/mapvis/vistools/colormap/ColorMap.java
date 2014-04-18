57cfd68318e5df79b608e075e9a0e18aa1733401
Yang Muye <muye.yang@gmail.com>
2014-04-18 16:09:18 +0800

    revised color map

--- vistools/src/main/java/mapvis/vistools/colormap/ColorMap.java
+++ vistools/src/main/java/mapvis/vistools/colormap/ColorMap.java
@@ -2,8 +2,34 @@ package mapvis.vistools.colormap;
 
 import java.awt.*;
 
-abstract class ColorMap {
-    abstract Color getColor(double value);
+public abstract class ColorMap {
+    public abstract Color getColor(double value);
 
-    static final ColorMap JET = new JetColorMap();
+    public static final ColorMap JET = new GenericColorMap(
+            new Color[]{Color.black, Color.blue, Color.cyan,
+                    Color.yellow, Color.red, Color.black},
+            -0.125f, 1.125f);
+
+    public static  final ColorMap hsv = new GenericColorMap(
+            new Color[]{Color.red, Color.yellow, Color.green,
+                    Color.cyan, Color.blue, Color.magenta, Color.red});
+
+    public static  final ColorMap hot = new GenericColorMap(
+            new Color[]{Color.black, Color.red, Color.yellow, Color.white});
+
+    public static  final ColorMap cool = new GenericColorMap(
+            new Color[]{ Color.cyan, Color.magenta});
+
+    public static  final ColorMap summer = new GenericColorMap(
+            new Color[]{ new Color(0.0f, 0.5f, 0.4f), new Color(1.0f, 1.0f, 0.4f)});
+
+    public static  final ColorMap pink = new GenericColorMap(
+            new Color[]{ new Color(255, 128, 128), Color.white});
+
+    public static  final ColorMap gray = new GenericColorMap(
+            new Color[]{ Color.black, Color.white});
+
+    public static  final ColorMap white = new GenericColorMap(
+            new Color[]{ Color.white, Color.white});
+
 }
