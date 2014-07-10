ca985b04a7f596c652cb150da59daaf702c5d665
Yang Muye <muye.yang@gmail.com>
2014-07-11 02:13:42 +0800

    added: CoastCache

---
+++ grid/src/main/java/mapvis/algo/CoastCache.java
@@ -0,0 +1,59 @@
+package mapvis.algo;
+
+import mapvis.grid.Dir;
+import mapvis.grid.Grid;
+import mapvis.grid.Tile;
+import mapvis.tree.MPTT;
+
+import java.util.*;
+
+public class CoastCache<T> {
+
+    Map<T, Set<Tile<T>>> m = new HashMap<>();
+
+    public Grid<T> grid;
+    public MPTT<T> tree;
+
+
+    public void insertAffect(int x, int y, T o){
+        Tile<T> t = grid.getTile(x, y);
+        this.recursivelyAffect(t);
+
+        Set<Tile<T>> neighbours = grid.getNeighbours(t.getX(), t.getY());
+        neighbours.forEach(this::recursivelyAffect);
+    }
+
+
+    private void recursivelyAffect(Tile<T> t) {
+        List<T> pathToNode = tree.getPathToNode(t.getObj());
+        for (T t1 : pathToNode) {
+            affect(t1, t);
+        }
+    }
+
+    private void affect(T o, Tile<T> t){
+        Set<Tile<T>> l = getCoastList(o);
+
+        Set<Tile<T>> neighbours = grid.getNeighbours(t.getX(), t.getY());
+
+        // surrounded by anything other than its decedents
+        boolean isCoast = !neighbours.stream()
+                .allMatch(n -> tree.isAncestorOf(o, n.getObj()));
+
+        if (isCoast)
+            l.add(t);
+        else
+            l.remove(t);
+    }
+
+
+
+
+    private Set<Tile<T>> getCoastList(T o){
+        Set<Tile<T>> list = m.get(o);
+        if (list==null)
+            m.put(o, list = new HashSet<>());
+        return list;
+    }
+
+}
