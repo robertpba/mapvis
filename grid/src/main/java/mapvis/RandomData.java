80f8665f2b64d36f86407c95bde3c09af23e37dd
Yang Muye <muye.yang@gmail.com>
2014-07-20 12:05:00 +0800

    generate random data based on parameters

--- grid/src/main/java/mapvis/RandomData.java
+++ grid/src/main/java/mapvis/RandomData.java
@@ -11,21 +11,28 @@ public class RandomData {
     public static MPTT<Integer> getTree(){
         MPTT<Integer> mptt = new MPTT<>();
         mptt.setRoot(lastId = 0);
-        getTree(mptt, 0, 0);
+        getTree(mptt, 0, 0, 2, 10, 100);
         mptt.refresh();
         return mptt;
     }
 
-    static void getTree(MPTT<Integer> tree, Integer node, int level){
-        if (level == 3)
+    static void getTree(MPTT<Integer> tree, Integer node, int level, int maxLevel, int span, int weight){
+        if (level > maxLevel)
             return ;
 
-        int n = rn.nextInt(10);
+        int n = rn.nextInt(span);
 
         while (n-- > 0){
             int cid = ++lastId;
-            tree.addChild(node, cid, rn.nextInt(100));
-            getTree(tree, cid, level+1);
+            tree.addChild(node, cid, rn.nextInt(weight));
+            getTree(tree, cid, level+1, maxLevel, span, weight);
         }
+    }
+    public static MPTT<Integer> getTree(int level, int span, int weight){
+        MPTT<Integer> mptt = new MPTT<>();
+        mptt.setRoot(lastId = 0);
+        getTree(mptt, 0, 0, level, span, weight);
+        mptt.refresh();
+        return mptt;
     }
 }
