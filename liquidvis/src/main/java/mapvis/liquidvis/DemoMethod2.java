4afbc784f97806681b3794f24924757dff576d53
Muye YANG <muye.yang@gmail.com>
2015-01-29 17:32:36 +0800

    PEA now works with YAML

--- liquidvis/src/main/java/mapvis/liquidvis/DemoMethod2.java
+++ liquidvis/src/main/java/mapvis/liquidvis/DemoMethod2.java
@@ -1,62 +1,92 @@
-//package mapvis.liquidvis;
-//
-//import mapvis.common.datatype.TreeImp;
-//import mapvis.liquidvis.gui.Observer;
-//import mapvis.liquidvis.method.method2.Method2;
-//import mapvis.liquidvis.model.MapModel;
-//import mapvis.liquidvis.model.handler.CollectStatistics;
-//import mapvis.liquidvis.util.LegacySeparateTextFileTreeLoader;
-//import mapvis.liquidvis.util.Node;
-//
-//import java.awt.image.BufferedImage;
-//import java.io.FileNotFoundException;
-//import java.io.IOException;
-//import java.util.Arrays;
-//
-//public class DemoMethod2 {
-//
-//    public static void main (String[] args) throws IOException, InterruptedException {
-//        LegacySeparateTextFileTreeLoader loader = new LegacySeparateTextFileTreeLoader();
-//        try {
-//            loader.load(
-//                    "data/finalPointInfo.txt",
-//                    "data/edgeInfo.txt",
-//                    "data/categoryName.txt");
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//
-//        Node geography = Arrays.stream(loader.root.children)
-//                .filter(n -> n.name.contains("Geography"))
-//                .findFirst().get();
-//
-//        Node byCountry = Arrays.stream(geography.children)
-//                .filter(n -> n.name.contains("Geography by country"))
-//                .findFirst().get();
-//
-//        geography.children = new Node[] { byCountry };
-//
-//        loader.root.children = new Node[]{ geography};
-//
-//
-//        MapModel model = new MapModel(TreeImp.from(loader.root));
-//        Method2 method = new Method2(model);
-//
-//        model.listeners.add(new CollectStatistics(model, 10));
-//
-//
-//        BufferedImage image = new BufferedImage(loader.width, loader.height+500, BufferedImage.TYPE_INT_RGB);
-//                Observer observer = new Observer(image, model);
-//        observer.imageUpdater.mapPolygonFillingColor = c ->
-//            colormap((float)interpolate(c.mass - c.area, 0.0, 0.0, 2000.0, 1.0) );
-//        observer.Start();
-//
-//
-//        method.IterateUntilStable(10000);
-//
-//        System.gc();
-//
-//        //observer.save("output/save.png");
-//    }
-//}
-//
+package mapvis.liquidvis;
+
+import mapvis.common.datatype.TreeImp;
+import mapvis.liquidvis.gui.Visualization;
+import mapvis.liquidvis.gui.actions.*;
+import mapvis.liquidvis.method.method2.Method2;
+import mapvis.liquidvis.model.*;
+import mapvis.liquidvis.model.handler.CollectStatistics;
+import mapvis.vistools.colormap.ColorMap;
+import org.yaml.snakeyaml.Yaml;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+
+import java.awt.image.BufferedImage;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.function.Function;
+
+import mapvis.common.datatype.Node;
+
+import static mapvis.vistools.Helper.interpolate;
+
+public class DemoMethod2 {
+
+    public static void main (String[] args) throws IOException, InterruptedException {
+        Yaml yaml = new Yaml();
+        Node node = yaml.loadAs(
+                new FileInputStream("liquidvis/data/tree1.yaml"), Node.class);
+
+        MapModel<Node> model = new MapModel<>(TreeImp.from(node), new MapModel.ToInitialValue<Node>() {
+            @Override
+            public Point2D getPosition(Node n) {
+                return new Point2D.Double((double)n.getVal("x")/10, (double)n.getVal("y")/10);
+            }
+            @Override
+            public double getMass(Node n) {
+                return (int)n.getVal("articles")/10;
+            }
+        });
+
+
+        Method2 method = new Method2(model);
+
+        model.listeners.add(new CollectStatistics(model, 10));
+
+
+        BufferedImage image = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);
+
+        Visualization visualization = new Visualization(image, model);
+
+        visualization.backgroundColor = Color.decode("#aaffff");
+
+        int[] nlevels = {0,1,4,16,64,256,1024};
+        String[] ncolors = {"#ffffff","#aae8ff", "#ffff33", "#ffcc00", "#ff9900", "#ff6600", "#cc3300", "#990000"};
+        Color[] colors = new Color[ncolors.length];
+        for (int i = 0; i < ncolors.length; i++) {
+            colors[i] = Color.decode(ncolors[i]);
+        }
+
+        final Function<Node, Color> colorMap1 = c -> {
+            mapvis.liquidvis.model.Polygon polygon = model.getPolygon(c);
+            double v = interpolate(((polygon.mass - polygon.area)/polygon.mass), 0.0, 0.5, 1.0, 1.0);
+            return ColorMap.JET.getColor(v);
+        };
+
+        model.actions.add(new LevelEncoder<>(model));
+        //model.actions.add(new EncodeLabelText<>(model, Node::getLabel));
+        model.actions.add(new CreateAreas<>(model));
+
+
+
+
+        //model.actions.add(new FillNode<>(model, colorMap2));
+
+        RenderBoundary<Node> renderBoundary = new RenderBoundary<>(model);
+
+        model.actions.add(renderBoundary);
+        //model.actions.add(new RenderOriginCentroid<>(model));
+        //model.actions.add(new LabelRender<>(model));
+
+        visualization.Start();
+
+        method.IterateUntilStable(10000);
+
+
+        System.gc();
+
+        //observer.save("output/save.png");
+    }
+}
+
