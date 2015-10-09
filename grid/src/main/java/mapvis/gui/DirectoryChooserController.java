cbbc06729e8e05a44fa1ca0cf699245f1fecd580
Martin Schwoerer <schwoere@in.tum.de>
2015-10-09 18:12:12 +0800

    choosing custom directory for tree generation

--- grid/src/main/java/mapvis/gui/DirectoryChooserController.java
+++ grid/src/main/java/mapvis/gui/DirectoryChooserController.java
@@ -3,8 +3,12 @@ package mapvis.gui;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.FileChooser;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -12,12 +16,12 @@ import java.util.ResourceBundle;
  * Created by dacc on 10/9/2015.
  */
 public class DirectoryChooserController implements Initializable {
-    @FXML
-    VBox vBox;
 
-//    void setVisible(boolean isVisible){
-//        vBox.setVisible(isVisible);
-//    }
+    @FXML
+    private TextField selectedDirectoryTextfield;
+
+    @FXML
+    private VBox vBox;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -25,14 +29,15 @@ public class DirectoryChooserController implements Initializable {
     }
 
     public void onSelectDirectory(ActionEvent event) {
-        System.out.println("onSelectDir");
+        DirectoryChooser directoryChooser = new DirectoryChooser();
+        directoryChooser.setTitle("Select the Directory to visualize");
+        File selectedFolder = directoryChooser.showDialog(vBox.getScene().getWindow());
+        System.out.printf("Path:" + selectedFolder.getPath());
+        selectedDirectoryTextfield.setText(selectedFolder.getPath());
     }
 
     void setVisible(boolean isVisible){
         vBox.setVisible(isVisible);
         vBox.setManaged(isVisible);
-//        generateTreeButton.setVisible(isVisible);
-//        generateTreeButton.setManaged(isVisible);
-
     }
 }
--- grid/src/main/java/mapvis/gui/InputController.java
+++ grid/src/main/java/mapvis/gui/InputController.java
@@ -7,10 +7,7 @@ import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import mapvis.Impl.HashMapGrid;
@@ -50,35 +47,30 @@ public class InputController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        inputSourceComboBox.getItems().addAll("Test1", "Test2");
-//        FXMLLoader loader = new FXMLLoader(getClass().getResource("setting.fxml"));
-//        settingController = loader.getController();
+        inputSourceComboBox.getItems().addAll("Filesystem", "RandomGenerator");
+        inputSourceComboBox.getSelectionModel().select(0);
+    }
 
+    private void selectSettingsController(boolean isSettingsControllerSelected)
+    {
+        if(isSettingsControllerSelected){
+            settingController.setVisible(true);
+            directoryController.setVisible(false);
+        }else{
+            settingController.setVisible(false);
+            directoryController.setVisible(true);
+        }
     }
 
     public void onSelectionChanged(ActionEvent event) {
         System.out.println("selection changed to " + inputSourceComboBox.getSelectionModel().getSelectedItem());
-        if(inputSourceComboBox.getSelectionModel().getSelectedItem().equals("Test1")){
-            settingController.setVisible(true);
-            directoryController.setVisible(false);
-//            inputSourceContainer.getChildren().clear();
-//            inputSourceContainer.getChildren().add(settingController);
-
-//            settingController.setVisible(true);
-//            directoryController.setVisible(false);
+        if(inputSourceComboBox.getSelectionModel().getSelectedItem().equals("Filesystem")){
+            selectSettingsController(false);
         }else{
-            settingController.setVisible(false);
-            directoryController.setVisible(true);
-//            inputSourceContainer.getChildren().clear();
-//            inputSourceContainer.getChildren().add(directoryController);
-//            inputSourceContainer.setVisible(false);
-
-//            settingController.setVisible(false);
-//            directoryController.setVisible(true);
+            selectSettingsController(true);
         }
     }
 
-    //
     @FXML
     public void begin(ActionEvent event) {
 
