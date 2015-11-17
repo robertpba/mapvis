352392afbbe8c685725c8402b409e517bf1ddccc
Martin Schwoerer <schwoere@in.tum.de>
2015-11-17 13:54:11 +0800

    rendering the boundary of regions as closed bezier curvature

--- grid/src/main/java/mapvis/graphic/RegionAreaRenderer.java
+++ grid/src/main/java/mapvis/graphic/RegionAreaRenderer.java
@@ -22,14 +22,14 @@ public class RegionAreaRenderer {
     private final GraphicsContext graphicsContext;
     private final IRegionPathGenerator regionBoundaryPointsGenerator;
     private final BiConsumer<Point2D[], Boolean> directPolyLineRenderer;
-    private final Consumer<Point2D[]> bezierCurveRenderer;
+    private final Consumer<List<Point2D[]>> bezierCurveRenderer;
     private final IRegionPathGenerator originalBoundaryPointsGenerator;
 
     public RegionAreaRenderer(GraphicsContext graphicsContext) {
         this.graphicsContext = graphicsContext;
-        this.regionBoundaryPointsGenerator = new SimplyfiedRegionPathGenerator(graphicsContext);
+//        this.regionBoundaryPointsGenerator = new SimplyfiedRegionPathGenerator(graphicsContext);
 //        this.regionBoundaryPointsGenerator = new DirectRegionPathGenerator(graphicsContext);
-//        this.regionBoundaryPointsGenerator = new MovingAverageRegionPathGenerator(2);
+        this.regionBoundaryPointsGenerator = new MovingAverageRegionPathGenerator(2);
         this.originalBoundaryPointsGenerator = new DirectRegionPathGenerator(graphicsContext);
 
 
@@ -44,10 +44,10 @@ public class RegionAreaRenderer {
         };
 
         bezierCurveRenderer = (shapePointArr) -> {
-            if(shapePointArr.length < 4)
-                return;
+//            if(shapePointArr.length < 4)
+//                return;
 
-            drawSpline(graphicsContext, shapePointArr, 0.2);
+            drawSpline(graphicsContext, shapePointArr, 0.3);
         };
     }
 
@@ -73,12 +73,12 @@ public class RegionAreaRenderer {
             List<Point2D[]> shapePoints = regionBoundaryPointsGenerator.drawRegionPaths(regionBoundaryShape);
             boolean firstDraw = true;
 
-            
-            for (Point2D[] shapePointArr : shapePoints) {
-//                this.directPolyLineRenderer.accept(shapePointArr, firstDraw);
-                firstDraw = false;
-                this.bezierCurveRenderer.accept(shapePointArr);
-            }
+            this.bezierCurveRenderer.accept(shapePoints);
+//            for (Point2D[] shapePointArr : shapePoints) {
+////                this.directPolyLineRenderer.accept(shapePointArr, firstDraw);
+//                firstDraw = false;
+//                this.bezierCurveRenderer.accept(shapePointArr);
+//            }
 
 //            shapePoints = originalBoundaryPointsGenerator.drawRegionPaths(regionBoundaryShape);
 //            for (Point2D[] shapePointArr : shapePoints) {
@@ -94,117 +94,52 @@ public class RegionAreaRenderer {
 //        graphicsContext.fill();
     }
 
-    void drawSpline(GraphicsContext ctx, Point2D[] points, double t) {
+    void drawSpline(GraphicsContext ctx, List<Point2D[]> input, double t) {
+        List<Point2D> cp = new ArrayList<>();   // array of control points, as x0,y0,x1,y1,...
 
-        if(true){
-            List<Point2D> cp = new ArrayList<>();   // array of control points, as x0,y0,x1,y1,...
+        List<Point2D> inputPointList = new ArrayList<>();
+        Point2D[] lastBoundaryShape = input.get(input.size() - 1);
+        Point2D lastPoint = lastBoundaryShape[lastBoundaryShape.length - 1];
+        inputPointList.add(lastPoint);
 
-            int n = points.length;
-
-            for (int i = 0; i < n - 2; i++) {
-                Pair<Point2D, Point2D> controlPoints = getControlPoints(points[i], points[i + 1], points[i + 2], t);
-                cp.add(controlPoints.getKey());
-                cp.add(controlPoints.getValue());
+        for (int i = 0; i < input.size(); i++) {
+            Point2D[] point2Ds = input.get(i);
+            for (int j = 0; j < point2Ds.length; j++) {
+                inputPointList.add(point2Ds[j]);
             }
-
-            Point2D[] controlPoints = cp.toArray(new Point2D[cp.size()]);
-
-            ctx.beginPath();
-            ctx.moveTo(points[0].getX(), points[0].getY());
-            ctx.quadraticCurveTo(controlPoints[0].getX(), controlPoints[0].getY(), points[1].getX(), points[1].getY());
-            ctx.stroke();
-
-            for (int j = 1; j < n - 3; j++) {
-//                ctx.beginPath();
-                ctx.moveTo(points[j].getX(), points[j].getY());
-
-                Point2D controlPoint1 = controlPoints[2 * j - 1];
-                Point2D controlPoint2 = controlPoints[2 * j];
-
-                ctx.bezierCurveTo(controlPoint1.getX(), controlPoint1.getY(), controlPoint2.getX(), controlPoint2.getY(), points[j + 1].getX(), points[j + 1].getY());
-//                ctx.stroke();
-            }
-
-//            ctx.beginPath();
-            ctx.moveTo(points[n - 1].getX(), points[n - 1].getY());
-            ctx.quadraticCurveTo(controlPoints[controlPoints.length - 2].getX(), controlPoints[controlPoints.length - 2].getY(), points[n - 2].getX(), points[n - 2].getY());
-
-            ctx.stroke();
-        }else {
-
-            List<Point2D> cp = new ArrayList<>();   // array of control points, as x0,y0,x1,y1,...
-            int n = points.length;
-
-            for (int i = 0; i < n - 2; i++) {
-                Pair<Point2D, Point2D> controlPoints = getControlPoints(points[i], points[i + 1], points[i + 2], t);
-                cp.add(controlPoints.getKey());
-                cp.add(controlPoints.getValue());
-            }
-
-            Point2D[] controlPoints = cp.toArray(new Point2D[cp.size()]);
-
-            for (int j = 1; j < n - 3; j++) {
-                ctx.beginPath();
-                ctx.moveTo(points[j].getX(), points[j].getY());
-
-                Point2D controlPoint1 = controlPoints[2 * j - 1];
-                Point2D controlPoint2 = controlPoints[2 * j];
-
-                ctx.bezierCurveTo(controlPoint1.getX(), controlPoint1.getY(), controlPoint2.getX(), controlPoint2.getY(), points[j + 1].getX(), points[j + 1].getY());
-                ctx.stroke();
-            }
-
-            ctx.beginPath();
-            ctx.moveTo(points[0].getX(), points[0].getY());
-            ctx.quadraticCurveTo(controlPoints[0].getX(), controlPoints[0].getY(), points[1].getX(), points[1].getY());
-            ctx.stroke();
-
-
-            ctx.beginPath();
-            ctx.moveTo(points[n - 1].getX(), points[n - 1].getY());
-            ctx.quadraticCurveTo(controlPoints[controlPoints.length - 2].getX(), controlPoints[controlPoints.length - 2].getY(), points[n - 2].getX(), points[n - 2].getY());
-            ctx.stroke();
         }
-    }
 
-//
-//    void drawSpline(Point2D[] inputPoints, double t) {
-//
-//        List<Point2D> cp = new ArrayList<>();   // array of control points, as x0,y0,x1,y1,...
-//        int n = inputPoints.length;
-//
-//        // Draw an open curve, not connected at the ends
-//        for (int i = 0; i < n - 2; i ++) {
-//            Pair<Point2D, Point2D> controlPoints = getControlPoints(inputPoints[i], inputPoints[i + 1], inputPoints[i + 2], t);
-//            cp.add(controlPoints.getKey());
-//            cp.add(controlPoints.getValue());
-//        }
-//        Point2D[] controlPoints = cp.toArray(new Point2D[cp.size()]);
-//
-//        for (int i = 1; i < n - 2; i += 1) {
-//
-//            graphicsContext.beginPath();
-//            Point2D startPoint = inputPoints[i];
-//            graphicsContext.moveTo(startPoint.getX(), startPoint.getY());
-//
-//            Point2D controlPoint1 = controlPoints[i - 1];
-//            Point2D controlPoint2 = controlPoints[i];
-//
-//            Point2D endPoint = inputPoints[i + 1];
-//            graphicsContext.bezierCurveTo(controlPoint1.getX(), controlPoint1.getY(), controlPoint2.getX(), controlPoint2.getY(), endPoint.getX(), endPoint.getY());
-//
-//            graphicsContext.stroke();
-//            graphicsContext.closePath();
-//        }
-//
-////        graphicsContext.beginPath();
-////        graphicsContext.moveTo(inputPoints[0].getX(), inputPoints[0].getY());
-////        graphicsContext.quadraticCurveTo(controlPoints[0].getX(), controlPoints[0].getY(), inputPoints[1].getX(), inputPoints[1].getY());
-////        graphicsContext.stroke();
-////        graphicsContext.closePath();
-//
-////        graphicsContext.restore();
-//    }
+        Point2D firstPoint = inputPointList.get(0);
+        inputPointList.add(firstPoint);
+
+        Point2D[] points = inputPointList.toArray(new Point2D[inputPointList.size()]);
+        int n = points.length;
+
+        for (int i = 0; i < n - 2; i++) {
+            Pair<Point2D, Point2D> controlPoints = getControlPoints(points[i], points[i + 1], points[i + 2], t);
+            cp.add(controlPoints.getKey());
+            cp.add(controlPoints.getValue());
+        }
+        cp.add(cp.get(cp.size() - 2));
+        cp.add(cp.get(cp.size() - 1));
+
+        Point2D[] controlPoints = cp.toArray(new Point2D[cp.size()]);
+
+        ctx.beginPath();
+
+        for (int j = 1; j < n - 1; j++) {
+            if(j == 1){
+                ctx.moveTo(points[j].getX(), points[j].getY());
+            }
+
+            Point2D controlPoint1 = controlPoints[2 * j - 1];
+            Point2D controlPoint2 = controlPoints[2 * j];
+
+            ctx.bezierCurveTo(controlPoint1.getX(), controlPoint1.getY(), controlPoint2.getX(), controlPoint2.getY(), points[j + 1].getX(), points[j + 1].getY());
+
+        }
+        ctx.fill();
+    }
 
     private Pair<Point2D, Point2D> getControlPoints(Point2D x0, Point2D x1, Point2D x2, double t) {
         //  x0,y0,x1,y1 are the coordinates of the end (knot) pts of this segment
